c2ec85e6759c7714b2f875d4452a0c1fd437a3f2
Sergei <nickincorname@gmail.com>
2021-10-24 23:20:34 +0300

    added notification

--- app/src/main/java/com/example/musicbox/MainActivity.kt
+++ app/src/main/java/com/example/musicbox/MainActivity.kt
@@ -1,14 +1,51 @@
 package com.example.musicbox
 
+import android.app.Notification
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val NOTIFICATION_ID = 101
+        const val CHANNEL_ID = "channelID"
+        const val IDENTIFIER_STRING = "com.example.musicbox"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: LinearLayout = findViewById(R.id.notific)
+        button.setOnClickListener {
+            // Создаём уведомление
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_notification_important_24)
+                .setContentTitle("Напоминание")
+                .setContentText("Ничего не произошло")
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(NOTIFICATION_ID, builder.build()) // посылаем уведомление
+            }
+
+
+            val intent = Intent()
+            intent.setAction(IDENTIFIER_STRING)
+            intent.putExtra("text","у вас новое уведомление")
+            intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+            sendBroadcast(intent)
+
+        }
     }
 
 
@@ -20,5 +57,6 @@ class MainActivity : AppCompatActivity() {
             stopService(i);
         }
     }
+
 
 }
